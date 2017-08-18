56dfd21971892be55b1fbd25da26442ecd2054da
Leon <leon@Leons-MacBook-Pro.local>
2017-08-18 16:33:39 -0400

    first commit

---
+++ src/main/java/io/zipcoder/drinks_machine_factory/MySpringApplication.java
@@ -0,0 +1,14 @@
+package io.zipcoder.drinks_machine_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by leon on 8/14/17.
+ */
+@SpringBootApplication
+public class MySpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringApplication.class, args);
+    }
+}
---
+++ src/test/java/io/zipcoder/drinks_machine_factory/MySpringApplicationTest.java
@@ -0,0 +1,7 @@
+package io.zipcoder.drinks_machine_factory;
+
+/**
+ * Created by leon on 8/18/17.
+ */
+public class MySpringApplicationTest {
+}
